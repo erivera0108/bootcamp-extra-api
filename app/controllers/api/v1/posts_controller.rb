d38cf8bffb9451147d07a1679a4f0a68838e56fc
Edgar <edgar.a.rivera08@gmail.com>
2020-06-14 22:49:26 -0400

    created show action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,14 @@ class Api::V1::PostsController < ApplicationController
 
         render json: posts
     end
+    
+    def show
+        post = Post.find(params[:id])
+
+        render json: post
+    end
+
+    # def create
+        
+    # end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :topics
     has_many :tags
+
 end
