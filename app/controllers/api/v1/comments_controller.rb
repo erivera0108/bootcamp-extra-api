84feb8f60e81e3931977d73512178c6174356c1c
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-17 12:06:19 -0400

    Comments Updated

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,4 +5,29 @@ class Api::V1::CommentsController < ApplicationController
 
         render json: comments
     end
+    def show
+        comment = Comment.find(params[:id])
+
+        render json: comment
+    end
+
+    def create
+        comments = Comment.create(comments_params)
+
+        render json: comments
+    end
+
+    def update
+        comments = Comment.find(params[:id])
+
+        comments.update(comments_params)
+
+        render json: comments
+    end
+
+    private
+
+    def comments_params
+        params.require(:comments).permit(:text, :user_id, :post_id, :created_at, :updated_at)
+    end
 end
